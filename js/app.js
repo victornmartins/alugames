f0e97ad5e7c70e6b8d8bc5c881b2120e4ce7f1f3
Victor Martins <victornmartins@gmail.com>
2024-10-12 15:06:25 -0300

    add confirmacao

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@ function alterarStatus(item){
     let boardgame = document.getElementById(`game-${item}`);
     let imagem = boardgame.querySelector('div');
     let botao = boardgame.querySelector('a');
+    let nomeBoardgame = boardgame.querySelector('p');
+
     
     if(imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+
+         if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeBoardgame.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
 
     } else {
         imagem.classList.add('dashboard__item__img--rented');
